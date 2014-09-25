4c7896bcc5d29690ddf27578b73f884ba5dd5e5b
Pablo Somodi <pablo@somodi.com.ar>
2014-09-25 17:38:41 -0300

    aridades

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -43,10 +43,21 @@ class Mostrador:
     def __init__(self):
         self.window = visual.Window([800,600], monitor="testMonitor", units="deg")
 
-    def print_cards(imgs1y2, imgs3y4, imgs5y6):
+
+
+    def imprimir_cartas(imgs1y2, imgs3y4, imgs5y6):
         #imprime las cartas en orden en window
         img1, img2 = imgs1y2
         img3, img4 = imgs3y4
         img5, img6 = imgs5y6
-        
+
+
+def test():
+	mostrador = Mostrador()
+	par1 = ("cartas/5O.jpg", "cartas/6O.jpg")
+	par2 = ("cartas/5O.jpg", "cartas/6O.jpg")
+	par3 = ("cartas/5O.jpg", "cartas/6O.jpg")
+	mostrador.print_cards(par1, par2, par3)
+
+test()
 
